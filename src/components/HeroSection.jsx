600bdaf8312fbaa1d787ef5e8239fe2ed9b4d62e
Abdelrahman Mostafa <112385425+Abdouumostafa@users.noreply.github.com>
2023-08-06 12:30:55 +0300

    Add files via upload

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,62 +1,65 @@
 const HeroSection = () => {
-  return (<>
-    <div className="heroImg relative px-10 py-24 text-white">
-      <div className=" ">
-      <h1 className='text-[44px] font-bold z-20 relative'>إسم الشركة</h1>
-      <p className="text-[20px] relative z-20 sm:w-[40%] w-[90%]">نساعدك في العثور على الإطارات المناسبة لسياراتك بجانب اتخاذ الترتيبات اللازمة مع مراكز  التركيب المعتمدة لدينا بسعر تنافسي و
-شراء وتركيب  إطارات سيارتك الآن أصبح أسهل وأسرع وأقرب.</p>        
-      </div>    
-    </div>
-      <div className="mt-10 pb-20">
-        <h1 className="text-[40px] font-bold text-center mb-8">تركيب أطار جديد</h1>
-        <div className="flex flex-wrap justify-center items-center gap-8 text-center">
-          <div className="w-[300px]">
-          <label htmlFor="list" className="block font-bold text-[18px]">قائمة الاطارات المركبة على السيارات</label>
-          <select id="list" className="border focus:outline-0 p-2 mt-3">
-            <option >إطار 1</option>
-            <option >إطار 2 </option>
-            <option >إطار 3</option>
-          </select>            
-          </div>
-          <div className="w-[300px]">
-          <label htmlFor="list" className="block font-bold text-[18px]">تسجيل قراءة لعداد السيارة</label>
-          <input type="number" id="list" className=" border border-black focus:outline-none p-2 mt-3"/>
-          </div>
-          <div className="w-[300px]">
-          <label htmlFor="list" className="block font-bold text-[18px]">موضع الاطار على السيارة</label>
-          <select id="list" className="border focus:outline-0 p-2 mt-3">
-            <option > R1</option>
-            <option > R2 </option>
-            <option > R3</option>
-            <option > L1</option>
-            <option > L2 </option>
-            <option > L3</option>
-          </select>            
-          </div>
-          <div className="w-[300px]">
-          <label htmlFor="list" className="block font-bold text-[18px]">حالة الاطار</label>
-          <select id="list" className="border focus:outline-0 p-2 mt-3">
-            <option >جديد</option>
-            <option > مستعمل </option>
-          </select>            
-          </div>
-          <div className="w-[300px]">
+  return (
+    <div className="mt-10 pb-20 text-center">
+        <h1 className="title relative text-[40px] font-bold text-center mb-8">تركيب أطار جديد</h1>
           <label htmlFor="list" className="block font-bold text-[18px]">تاريخ تركيب الاطار</label>
           <input type="date" id="list" />
-          </div>
+          <h2 className="mt-12 relative text-[40px] font-bold text-center mb-8">معلومات السيارة</h2>
+          <div className="flex flex-wrap justify-center items-center gap-14">
+            <div className="w-[300px]">
+              <label className="block mb-3 text-[20px]">رقم السيارة</label>
+              <input type="text" className="p-3 text-center border border-black focus:outline-none" placeholder="أ ب ج 000"/>
+            </div>
+            <div className="w-[300px]">
+              <label className="block mb-3 text-[20px]">عداد السيارة</label>
+              <input type="number" className="p-3 text-center border border-black focus:outline-none" placeholder="123456"/>
+            </div>
+            <div className="w-[300px]">
+              <label className="block mb-3 text-[20px]"> الإطار المراد تغييره</label>
+              <select className="p-2 focus:outline-none">
+                <option>الإطار 1 </option>
+                <option>الإطار 2</option>
+                <option>الإطار 3</option>
+              </select>
+            </div>
+            <div className="w-[300px]">
+              <label className="block mb-3 text-[20px]">   موضع الإطار</label>
+              <select className="p-2 focus:outline-none">
+                <option>R1</option>
+                <option>L1</option>
+                <option>R2</option>
+                <option>L2</option>
+                <option>R3</option>
+                <option>L3</option>
+              </select>
+            </div>
           <div className="w-[300px]">
-          <label htmlFor="list" className="block font-bold text-[18px]">الشركة المالكة للسيارة</label>
-          <select id="list" className="border focus:outline-0 p-2 mt-3">
-            <option > الشركة 1</option>
-            <option > الشركة 2 </option>
-            <option >الشركة 3</option>
-            <option >الشركة 4</option>
+              <label className="block mb-3 text-[20px]">   حالة الإطار القديم</label>
+              <select className="p-2 focus:outline-none">
+                <option>  جديد</option>
+                <option> مستعمل</option>
+              </select>
+            </div>
+            <div className="w-[300px]">
+              <label className="block mb-3 text-[20px]">الرقم التسلسلي للإطار (الباركود)</label>
+              <input type="number" className="p-3 text-center border border-black focus:outline-none" placeholder="74567543"/>
+            </div>
+            <div className="w-[300px]">
+            <label className="block mb-3 text-[20px]">إحالة الإطار القديم إلى:</label>
+            <div className="flex flex-col">
+              <div>
+            <label htmlFor="1" className="mx-4 text-[20px]">بيع اتلاف</label>
+            <input type="radio" name="option" id="1"/>                
+              </div>
+              <div>
+            <label htmlFor="2" className="mx-2 text-[20px]">اعادة للوكيل</label>
+            <input type="radio" name="option" id="2"/>                
+              </div>
 
-          </select>            
+            </div>
+            </div>
           </div>
-        </div>
-      </div>
-</>
+    </div>
   )
 }
 export default HeroSection
